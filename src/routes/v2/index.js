be2f3949887a407971b793544210d71c44014da9
Rohan Itankar <rohanitu@gmail.com>
2025-07-08 18:24:59 +0530

    update readme

---
+++ src/routes/v2/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/info',(req,res) =>{
+    return res.json({
+        msg: "coming from v2 API",
+    });
+});
+
+module.exports = router;
